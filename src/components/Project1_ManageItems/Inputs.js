76388d0232fcefd1380bd7df1a9e543babd8013d
arislazaridis <arislazaridis@yahoo.com>
2021-10-24 23:35:51 +0300

    react project

--- src/components/Project1_ManageItems/Inputs.js
+++ src/components/Project1_ManageItems/Inputs.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function List({ items, removeItem, clearItems }) {
+function List({ items, removeItem, clearItems, editItems }) {
   return (
     <div>
       {items.map((item) => {
@@ -20,6 +20,9 @@ function List({ items, removeItem, clearItems }) {
             </p>
             <button type="button" onClick={() => removeItem(id)}>
               delete
+            </button>
+            <button type="button" onClick={() => editItems(id)}>
+              edit
             </button>
           </div>
         );
@@ -42,18 +45,41 @@ function Input({ value, handleInput }) {
 function Inputs() {
   const [item, SetItem] = useState();
   const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    if (item && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: item };
+          }
+          return item;
+        })
+      );
+      SetItem("");
+      setEditID(null);
+      setIsEditing(false);
+      alert(true, "success", "value changed");
+    }
     console.log(item);
     const newItem = { id: new Date().getTime().toString(), title: item };
     SetItem("");
+
     setList([...list, newItem]);
   };
 
   const removeItem = (id) => {
     setList(list.filter((item) => item.id !== id));
+  };
+
+  const editItems = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    SetItem(specificItem.title);
   };
 
   const clearItems = ({ list }) => {
@@ -70,7 +96,11 @@ function Inputs() {
         <ButtonInput type="submit" label="add" />
         {list.length > 0 && (
           <div>
-            <List items={list} removeItem={removeItem}></List>
+            <List
+              items={list}
+              removeItem={removeItem}
+              editItems={editItems}
+            ></List>
           </div>
         )}
       </div>
